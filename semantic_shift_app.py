de30d520214db72ee4950337ccf83c0edcbad2ed
Muhammad S. Abdo <Muhsabrys@outlook.com>
2025-11-23 23:45:31 -0500

    Update semantic_shift_app.py

--- semantic_shift_app.py
+++ semantic_shift_app.py
@@ -16,13 +16,7 @@ from sklearn.decomposition import PCA
 from scipy.spatial.distance import cosine
 
 import spacy
-try:
-    nlp = spacy.load("en_core_web_sm")
-except:
-    import subprocess
-    subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"])
-    nlp = spacy.load("en_core_web_sm")
-
+nlp = spacy.load("en_core_web_sm")
 
 # Ensure tokenizers + lemmatizer
 nltk.download("punkt")
